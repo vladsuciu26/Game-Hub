ab1fbaca60d02877ba1fa7927f334230a55aa5f5
vladsuciu26 <vladsuciu26@yahoo.com>
2024-10-22 23:11:06 +0300

    Add game card skeletons for loading screen.

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -24,20 +24,26 @@ interface FetchGameResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
 
+    setLoading(true);
     apiClient
       .get<FetchGameResponse>("/games", { signal: controller.signal })
-      .then((res) => setGames(res.data.results))
+      .then((res) => {
+        setGames(res.data.results)
+        setLoading(false);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
+        setLoading(false);
         setError(err.message);
       });
   }, []);
 
-  return { games, error };
+  return { games, error, isLoading };
 };
 
 export default useGames;
