2cd9bf4ee1100fe93025946fd1ce91d85be6fac3
vladsuciu26 <vladsuciu26@yahoo.com>
2024-10-22 21:55:54 +0300

    Platform icons and crop images.

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,32 @@
+import { FaWindows, FaPlaystation, FaXbox, FaApple, FaLinux, FaAndroid } from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { BsGlobe, BsNintendoSwitch } from "react-icons/bs";
+import { Platform } from "../hooks/useGames";
+import { HStack, Icon } from "@chakra-ui/react";
+import { IconType } from "react-icons";
+
+interface Props {
+    platforms: Platform[];
+}
+
+export const PlatformIconList = ({ platforms }: Props) => {
+    const iconMap: { [key: string]: IconType }= {
+        pc: FaWindows,
+        playstation: FaPlaystation,
+        xbox: FaXbox,
+        mac: FaApple,
+        linux: FaLinux,
+        android: FaAndroid,
+        ios: MdPhoneIphone,
+        nintendo: BsNintendoSwitch,
+        web: BsGlobe,
+    }
+
+    return (
+        <HStack marginY={"4px"}>
+            {platforms.map((platform) => (
+                <Icon as={iconMap[platform.slug]} color="gray.500"/>
+            ))}
+        </HStack>
+    )
+}
